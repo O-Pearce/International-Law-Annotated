dcf4b90751b226fdbbdd3b8a57b618fb1ce05bb0
O-Pearce <oscargpearce.1705@gmail.com>
2025-10-02 14:33:07 +1000

    modified:   .DS_Store deleted:    Notes modified:   cases/.DS_Store new file:   cases/nicaragua-germany-notes.js modified:   cases/nicaragua-germany.html new file:   cases/opt-advisory-opinion-notes.js modified:   cases/opt-advisory-opinion.html new file:   materials/old-nicaragua-germany.html new file:   materials/old-opt-advisory-opinion.html deleted:    nicaragua-germany-pretty-authors-chips.html deleted:    opt-opinion-pretty-authors-chips.html

---
+++ cases/nicaragua-germany-notes.js
@@ -0,0 +1,13 @@
+// Notes file generated from page. Add more annotations using addNote(...)
+const NOTES = {};
+function addNote(para, author, text, title="", source="") {
+  if (!NOTES[para]) NOTES[para] = [];
+  NOTES[para].push({ author, title, text, source });
+}
+
+addNote("para-2", "Oscar Pearce", "Note that Nicaragua has not strictly limited its claims to violations arising out of the post-October 7 2023 conflict. Rather, as is even more explicit in their application instituting proceedings, they consider Germany's pre-2023 support for Israel to also be unlawful. This position has been lended greater weight by the Advisory Opinion issued by the Court months later finding that Israel had perpetrated - and was continuing to perpetrate - various IHRL and IHL violations in the Occupied Palestinian Territory.", "", "");
+addNote("para-15", "Oscar Pearce", "It is notable that Germany here does not take the stronger position that Common Article 1 of the Geneva Conventions creates NO obligations with respect to other states. Those that adopt that more radical stance contend that the duty to ensure respect for IHL extends only to those under a state's direct influence (ie, individuals, corporations, etc).", "", "");
+addNote("para-18", "Oscar Pearce", "This decrease in exports from Germany reversed in the aftermath of this decision, which could undermine Germany's case at the merits stage of these proceedings. See Judge Tladi's declaration, and this article: https://www.justsecurity.org/114479/german-arms-exports-israel-icj/.", "", "");
+addNote("para-19", "Oscar Pearce", "Germany resumed funding for UNRWA shortly after these proceedings, which suggests this is unlikely to be an issue moving forward.", "", "");
+addNote("para-24", "Oscar Pearce", "Of greatest global relevance here is, of course, the Arms Trade Treaty, in particular Articles 6 (which prohibits certain transfers) and 7 (which imposes certain due diligence requirements.", "", "");
+addNote("para-25", "Oscar Pearce", "As to admissibility, the so-called 'Monetary Gold Principle' is likely to be at issue. Israel is not a party to these proceedings despite arguably having legal interests at stake (ie, in a hypothetical finding that it is committing certain violations).", "", "");
---
+++ cases/opt-advisory-opinion-notes.js
@@ -0,0 +1,12 @@
+// Notes file generated from page. Add more annotations using addNote(...)
+const NOTES = {};
+function addNote(para, author, text, title="", source="") {
+  if (!NOTES[para]) NOTES[para] = [];
+  NOTES[para].push({ author, title, text, source });
+}
+
+addNote("para-90", "Oscar Pearce", "This paragraph expresses the traditional test for applying the law of occupation. Even here, however, the phrase \"to the extent that\" a state exercises effective control signals the shift to functional interpretation below.", "", "");
+addNote("para-92", "Oscar Pearce", "The latter half of this paragraph reflects a shift towards the 'Functional Approach' to the law of occupation, supported by the ICRC and various academic/NGO commentators.", "", "");
+addNote("para-274", "Oscar Pearce", "It is notable, and perhaps disappointing that the Court framed this analysis in terms of  erga omnes  obligations rather than  jus cogens . See Judge Tladi's separate opinion for the argument that these consequences are properly attached to the latter.", "", "");
+addNote("para-278", "Oscar Pearce", "Where the next paragraph seems to draw on the duty in Article 41 of the ILC's Draft Articles on the Responsibility of States for Internationally Wrongful Acts, this paragraph instead echoes the obligations customarily associated with the specific prohibition on the acquisition of territory by force.", "", "");
+addNote("para-279", "Oscar Pearce", "Though the Court refrained from explicitly invoking it, these obligations seem to fit Article 41 of the ILC's Draft Articles on the Responsibility of States for Internationally Wrongful Act. Note that this provision applies to serious violations of peremptory norms.", "", "");
