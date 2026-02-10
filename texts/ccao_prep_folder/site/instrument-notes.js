f7cc962238655020f2af4ed919b063d817186a3b
O-Pearce <oscargpearce.1705@gmail.com>
2026-02-10 13:22:35 +1100

    modified:   index.html 	modified:   texts/.DS_Store 	new file:   texts/ccao_prep_folder/.DS_Store 	modified:   texts/ccao_prep_folder/instrument.csv 	new file:   texts/ccao_prep_folder/site/instrument-notes.js 	new file:   texts/ccao_prep_folder/site/instrument.html

---
+++ texts/ccao_prep_folder/site/instrument-notes.js
@@ -0,0 +1,11 @@
+window.NOTES = {
+  "para-001": [
+    {
+      "author": "Oscar Pearce",
+      "title": "",
+      "text": "Test",
+      "source": ""
+    }
+  ]
+};
+window.dispatchEvent(new Event('notes:ready'));
